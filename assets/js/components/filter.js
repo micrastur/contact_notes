c71df6c78375058f1af39975e3fd73cd3c288ef1
micrastur <marina.lisovik@gmail.com>
2017-05-15 17:41:13 +0300

    Update filter.js

--- assets/js/components/filter.js
+++ assets/js/components/filter.js
@@ -7,23 +7,23 @@ export const Filter = (props) => {
             <div className="filter_btn"
                  data-category="filter"
                  data-option="visibility"
-                 onClick = {(e) => props.onUserClick(e, !props.value)}>
-                <i className="fa fa-filter" aria-hidden="true"></i>
+                 onClick = {(e) => props.onUserClick.btn(e.currentTarget, !props.value)}>
+                <i className="icon fa fa-filter" aria-hidden="true"></i>
             </div>
             <div className={"filter_type " + (props.value ? "active" : "hidden")}>
                 <h3>Sort By:</h3>
-                <div className="filter_category">
-                    <span className="filter_alphabet">
-                        <i className="fa fa-sort-alpha-asc" aria-hidden="true"></i> Alphabet
+                <div className="filter_category" onClick={(e) => props.onUserClick.sort(e)}>
+                    <span className="filter_item filter_item-active filter_alphabet" data-category="filter" data-option="sort" data-filter-type="alphabet">
+                        <i className="icon fa fa-sort-alpha-asc" aria-hidden="true"></i> Alphabet
                     </span>
-                    <span className="filter_age">
-                        <i className="fa fa-sort-numeric-asc" aria-hidden="true"></i> Age
+                    <span className="filter_item filter_age"  data-category="filter" data-option="sort" data-filter-type="age">
+                        <i className="icon fa fa-sort-numeric-asc" aria-hidden="true"></i> Age
                     </span>
-                    <span className="filter_group">
-                        <i className="fa fa-users" aria-hidden="true"></i> Group
+                    <span className="filter_item filter_group"  data-category="filter" data-option="sort" data-filter-type="group">
+                        <i className="icon fa fa-users" aria-hidden="true"></i> Group
                     </span>
-                    <span className="filter_country">
-                        <i className="fa fa fa-globe" aria-hidden="true"></i> Country
+                    <span className="filter_item filter_country"  data-category="filter" data-option="sort" data-filter-type="country">
+                        <i className="icon fa fa fa-globe" aria-hidden="true"></i> Country
                     </span>
                 </div>
                 <SubFilterList />
