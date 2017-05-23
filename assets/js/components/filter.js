e73df523c7c632ea9dd24a2a227818ead193f366
micrastur <marina.lisovik@gmail.com>
2017-05-23 18:27:41 +0300

    Update filter.js

--- assets/js/components/filter.js
+++ assets/js/components/filter.js
@@ -5,26 +5,25 @@ export const Filter = (props) => {
     return (
         <div className="filter float-l">
             <div className="filter_btn"
-                 data-category="filter"
-                 data-option="visibility"
+                 data-state-category="filter-visibility"
                  onClick = {(e) => props.onUserClick.btn(e.currentTarget, !props.value)}>
                 <i className="icon fa fa-filter" aria-hidden="true"></i>
             </div>
             <div className={"filter_type " + (props.value ? "active" : "hidden")}>
                 <h3>Sort By:</h3>
                 <div className="filter_category" onClick={(e) => props.onUserClick.sort(e)}>
-                    <span className="filter_item filter_item-active filter_alphabet" data-category="filter" data-option="sort" data-filter-type="alphabet">
+                    <label className="filter_item filter_alphabet" data-state-category="filter-method" data-status="main" data-type="alphabet">
                         <i className="icon fa fa-sort-alpha-asc" aria-hidden="true"></i> Alphabet
-                    </span>
-                    <span className="filter_item filter_age"  data-category="filter" data-option="sort" data-filter-type="age">
-                        <i className="icon fa fa-sort-numeric-asc" aria-hidden="true"></i> Age
-                    </span>
-                    <span className="filter_item filter_group"  data-category="filter" data-option="sort" data-filter-type="group">
+                    </label>
+                    <label className="filter_item filter_group"  data-state-category="filter-method" data-status="main" data-type="group">
                         <i className="icon fa fa-users" aria-hidden="true"></i> Group
-                    </span>
-                    <span className="filter_item filter_country"  data-category="filter" data-option="sort" data-filter-type="country">
+                    </label>
+                    <label className="filter_item filter_age"  data-state-category="filter-method" data-status="additional" data-type="age">
+                        <i className="icon fa fa-sort-numeric-asc" aria-hidden="true"></i> Age
+                    </label>
+                    <label className="filter_item filter_country"  data-state-category="filter-method" data-status="additional" data-type="country">
                         <i className="icon fa fa fa-globe" aria-hidden="true"></i> Country
-                    </span>
+                    </label>
                 </div>
                 <SubFilterList />
             </div>
