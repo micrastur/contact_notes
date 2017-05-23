838381ed505ceabef35ee3de75f2d4f58dd476d3
micrastur <marina.lisovik@gmail.com>
2017-05-23 18:31:08 +0300

    Update list.js

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,19 +1,15 @@
-/**
- * Created by micrastur on 19.02.2017.
- */
 import React from 'react';
 
-export default class CreateList extends React.Component {
-    render() {
-        return (
-            <div>
-                <h2>{this.props.value[0].group}</h2>
-                <ul>
-                    {this.props.value.map((value, index) =>
-                        <li key={"item-" + index}>{value.name + ' ' + value.surname}</li>
-                    )}
-                </ul>
-            </div>
-        )
-    }
+export default function CreateList(props) {
+    return (
+    //<div>
+    //    <h2>{this.props.value[0].group}</h2>
+    //    <ul>
+    //        {this.props.value.map((value, index) =>
+    //                <li key={"item-" + index}>{value.name + ' ' + value.surname}</li>
+    //        )}
+    //    </ul>
+    //</div>
+    <li>{props.value.name + ' ' + props.value.surname}</li>
+    )
 }
