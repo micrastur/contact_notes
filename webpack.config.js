543b50e46c4e6f64bde167742813d6cd9496534e
micrastur <marina.lisovik@gmail.com>
2017-07-27 16:45:54 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,11 @@ process.noDeprecation = true;
 
 module.exports = {
     context: __dirname + '/assets/',
-    entry: './js/app',
+    entry: {
+        app: './js/app'
+    },
     output: {
-        path: __dirname + '/source/',
+        path: path.resolve(__dirname, 'source'),
         publicPath: '/source/',
         filename: 'js/[name].js',
         library: '[name]'
@@ -18,12 +20,15 @@ module.exports = {
     watch: environment === 'dev',
     devtool: environment === 'dev' ? 'source-map' : '',
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.jsx?$/,
                 loader: "babel-loader",
-                exclude: /(node_modules|bower_components)/,
-                query: {
+                exclude: [
+                    path.resolve(__dirname, 'node_modules'),
+                    path.resolve(__dirname, 'bower_components')
+                ],
+                options: {
                     presets: ['es2015', 'react', 'stage-0', 'stage-1']
                 }
             },
@@ -33,12 +38,16 @@ module.exports = {
             },
             {
                 test: /\.(jpg|jpeg|gif|png|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
-                include: path.resolve(__dirname, 'node_modules'),
+                include: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
                 loader: 'file-loader?name=[1]&regExp=node_modules(.*)'
             },
             {
                 test: /\.(jpg|jpeg|gif|png|woff|woff2|eot|ttf|svg)(\?.*$|$)/,
-                exclude: path.resolve(__dirname, 'node_modules'),
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ],
                 loader: 'file-loader?name=[path][name].[ext]'
             }
         ]
