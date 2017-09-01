2e15d249499d66dbfa82ca8f0f50893c708a79b7
micrastur <marina.lisovik@gmail.com>
2017-09-01 10:49:44 +0300

    Update filter.js

--- assets/js/components/filter.js
+++ assets/js/components/filter.js
@@ -1,176 +1,40 @@
 import React from 'react';
-import "../../css/list.css";
-import PropTypes from 'prop-types';
+import "../../css/filter.css";
 
-export default class CreateList extends React.Component {
-
-    static propTypes = {
-        activeItem: PropTypes.string,
-        people: PropTypes.array,
-        search: PropTypes.string,
-        method: PropTypes.array,
-        onListClick: PropTypes.func
-    };
-
-    constructor(props){
-        super(props);
-        console.log(this.props);
-        this.listInfo = [
-            {
-                title: 'Personal Information',
-                labels: ['Name', 'Surname', 'Birth Date', 'Age', 'Gender', 'Group']
-            },
-            {
-                title: 'Contacts',
-                labels: ['Address', 'Country', 'E-mail', 'Phone']
-            }
-        ];
-        this.activateItem = this.activateItem.bind(this);
-    }
-
-    generateGroupLists(){
-        let groupList = this.people.map((value, item) =>
-                <div className={`group_item` + ` group_${value[0].group}`} key={value[0].group} >
-                    <h2 className="group_heading">{value[0].group}</h2>
-                    {this.getList(value, item)}
+export default function Filter(props){
+    return (
+        <div className="filter float-l">
+            <div className="filter_btn"
+                 data-state-category="filter-visibility"
+                 onClick = {(e) => props.onUserClick.btn(e.currentTarget, !props.value)}>
+                <i className="icon fa fa-filter" aria-hidden="true"></i>
+            </div>
+            <div className={"filter_type " + (props.value ? "active" : "hidden")}>
+                <h3>Sort By:</h3>
+                <div className="filter_category" onClick={(e) => {e.target !== e.currentTarget ? props.onUserClick.sort(e.target) : false}}>
+                    <label className="filter_item filter_alphabet filter_item-active" data-state-category="filter-method" data-type="alphabet">
+                        <i className="icon fa fa-sort-alpha-asc" aria-hidden="true"></i> Alphabet
+                    </label>
+                    <label className="filter_item filter_group"  data-state-category="filter-method" data-type="group">
+                        <i className="icon fa fa-users" aria-hidden="true"></i> Group
+                    </label>
+                    <label className="filter_item filter_age"  data-state-category="filter-method" data-type="age">
+                        <i className="icon fa fa-sort-numeric-asc" aria-hidden="true"></i> Age
+                    </label>
+                    <label className="filter_item filter_country"  data-state-category="filter-method" data-type="country">
+                        <i className="icon fa fa fa-globe" aria-hidden="true"></i> Country
+                    </label>
                 </div>
-        );
-        return (
-            <div className="group_list">
-                {groupList}
+                <SubFilterList />
             </div>
-        )
-    }
+        </div>
+    )
+};
 
-    activateItem(e){
-        let targetElement = e.target.tagName.toLowerCase() === 'li' ? e.target : e.target.closest('li'),
-            currentTarget = e.currentTarget;
-
-        if(targetElement !== currentTarget){
-            let activeListId = targetElement.id;
-            this.props.onListClick(targetElement, activeListId);
-        }
-    }
-
-    listHead(info){
-        console.log();
-        return (
-            <div className="list_item_head">
-                <img className="list_image" src={require(`../../img/people/${info.picture}`)} alt=""/>
-                <span className="list_title">
-                    {info.name + ' ' + info.surname}
-                    <span className="list_filter">
-                        {this.props.method[2] ? info[this.props.method[2]] : ''}
-                    </span>
-                </span>
-            </div>
-        )
-    }
-
-    listBody(info){
-        let labelList = (labels) => {
-                let lists = labels.map((name, item) => {
-                    return (
-                        <p key={`label_${item}`}>
-                            <span className="label_name">{`${name}:`}</span>
-                            {info[name.toLowerCase().split(/\s+|-/).join('_')]}
-                        </p>
-                    )
-                });
-                return (
-                    <div className="label_wrap">
-                        {lists}
-                    </div>
-                )
-            },
-            bodyInfo = this.listInfo.map((value, item) => {
-                let title = value.title,
-                    labels = value.labels;
-
-                return (
-                    <div className="item_info_type" key={`info_type-${item}`}>
-                        <h2 className="info_type_title">{title}</h2>
-                        {labelList(labels)}
-                    </div>
-                )
-            });
-        return (
-            <div className="list_item_body">
-                <div className="cf">
-                    <div className="item_info">
-                        <img className="item_image" src={require(`../../img/people/${info.picture}`)} alt=""/>
-                    </div>
-                    <div className="item_info personal_info cf">
-                        {bodyInfo}
-                    </div>
-                </div>
-                <div className="item_info_detail">
-                    <h2 className="info_type_title">About person</h2>
-                    <p>
-                        {info.about}
-                    </p>
-                </div>
-            </div>
-        )
-    }
-
-    getList(list, groupItem){
-        let currentList = list ? list : this.props.people,
-            activeItem = this.props.activeItem,
-            listItems = currentList.map((value, item) => {
-                    let curItem = groupItem ? groupItem : item,
-                        itemId = value._id,
-                        defaultOpenItem = activeItem === null  && curItem === 0 ? 'list_item-active' : '';
-                    return (
-                        <li key={`list-${itemId}`} id={itemId} className={'list_item ' + (activeItem === itemId ? 'list_item-active' : defaultOpenItem)} data-state-category="list-activeId">
-                            {this.listHead(value)}
-                            {this.listBody(value)}
-                        </li>
-                    )
-                }
-            );
-
-        return (
-            <ul onClick={this.activateItem}>
-                {listItems}
-            </ul>
-        )
-    }
-    getEmptyResult(){
-        return (
-            <div>
-                <p>The person
-                </p>
-            </div>
-        )
-    }
-
+class SubFilterList extends React.Component {
     render(){
-        let [data, groups, list] = [this.props, [], []],
-            filterByGroup = data.method.indexOf('group') !== -1,
-            foundedPeopleList = data.people;
-
-        if(filterByGroup){
-            for(let key of foundedPeopleList) {
-                let currentGroup = key.group,
-                    groupIndex = groups.indexOf(currentGroup);
-
-                groupIndex === -1
-                    ? (groups.push(currentGroup), list.push([key]))
-                    : list[groupIndex].push(key);
-            }
-            this.people = list;
-        }
         return (
-            <div className="list_container">
-                {
-                    foundedPeopleList.length > 0
-                    ? filterByGroup
-                        ? this.generateGroupLists()
-                        : this.getList()
-                    : this.getEmptyResult()
-                }
-            </div>
+            <div></div>
         )
     }
 }
